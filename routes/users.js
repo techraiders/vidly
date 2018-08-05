30b990f2c3bcb690ff34062eca4b9538d0bc2a61
techraiders <hello_navneet@hotmail.com>
2018-08-05 13:21:49 +0530

    Generates salt, and hashed password, saved user with hashed password.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -14,6 +15,9 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered');
 
   user = new User (_.pick(req.body, ['email', 'name', 'password']));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
   res.send(_.pick(user, ['_id', 'email', 'name']));
