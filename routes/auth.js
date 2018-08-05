bc603d91c21b9dd5d4b81eadadb517813eda6cfe
techraiders <hello_navneet@hotmail.com>
2018-08-05 14:46:51 +0530

    Generate and sends authentication token using JWT.

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const {User} = require('../models/user'); 
@@ -16,7 +17,9 @@ router.post('/', async (req, res) => {
 
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
+  const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+
+  res.send(token);
 });
 
 function validate (req) {
