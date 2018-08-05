deadbfe4fb7923bb66fdeffec0fc340d68b84398
techraiders <hello_navneet@hotmail.com>
2018-08-05 17:41:11 +0530

    Creates new protected routes that returns the information of current user.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
+const auth = require('../middleware/auth');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
