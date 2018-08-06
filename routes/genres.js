4eb90e72e2a87008afb8d3654beb1fe8203068f9
techraiders <hello_navneet@hotmail.com>
2018-08-06 23:23:45 +0530

    Integration test if get call to /api/genres/:id with invalid id returns with 404 status code.

--- routes/genres.js
+++ routes/genres.js
@@ -42,6 +42,9 @@ router.delete('/:id', [auth, admin], async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(404).send('Invalid id');
+  }
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
